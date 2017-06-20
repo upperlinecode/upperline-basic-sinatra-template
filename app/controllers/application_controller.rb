2fe9dfb7e2660f990636ff7b93688cb75ea50175
Anya Menon <anyamenon422@gmail.com>
2017-06-20 17:40:09 +0000

    hi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,27 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/encouragement' do
+    "Your shoes are super cool!"
+    "Your hair looks nice!"
+  end
+  
+  get '/insult' do
+    "Your feet are dumb"
+  end
+  
+  get '/math' do
+    @intro = "10 *490 is:"
+    @answer = 10 * 490
+    @answer.to_s
+  end
+  
+  post "/twistedsentence" do
+    @username = params["name"]
+    @userage = params["age"]
+    @fortune = "#{@username}, #{fortune(@userage)}"
+    erb :fortune
+  end
+  
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,19 @@
-class SampleModel
-  attr_accessor :name, :age
+def fortune(age)
+   if age.to_i > 25
+     return "you're old lol"
+   else
+     fortunes = [
+            "you will not die.",
+            "you will see the light in the room.",
+            "try harder.",
+            "don't believe in magical creatures because they are super not real.",
+            "tomorrow there will be death for everyone but people like me.",
+            "never look at your own face because it will disappoint you.",
+            "beware of the noises in the shower curtain.",
+            "when you poop in the garage die today.",
+            "unless you really want to eat shoes, don't breathe in mosquitoes.",
+            "always treasure a memory of your poop, and please leave."
+         ]
+         return fortunes.sample
+   end   
 end

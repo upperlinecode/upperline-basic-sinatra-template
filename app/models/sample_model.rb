fedbe893ac7f765ed8657ee08d565c4af194ecd4
Adrian Arnaboldi <aarnaboldi24@gmail.com>
2017-06-23 13:51:00 +0000

    new

--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -9,46 +9,50 @@ class Workout
         @topic = topic
         @wod = []
         @resttime = "error"
+        @movementscardio = ["lunges","goblet squats","burpees","mountain climbers","squat jumps","wallsit","jumping jacks","pushups","plank"]
+        @movementsstrength = ["plank","pushups"]
+        @movementsstrengthdumbells = ["dumbell snatches","kettle bell swings","hammer curls","tricep extension","bicep curls to shoulder press"]
+        @worktime = "error"
     end
     def rest 
         calcrest
-        @wod.push(@resttime + " rest")
+        @wod.push(@resttime + "seconds rest")
     end
-    def wodmaker
-        if @type == "strength"
-            @wod.push("1 minute alternating dumbell snatches")
-            @wod.push("30 seconds rest")
-            @wod.push("1 minute kettle bell swings")
-            @wod.push("30 seconds rest")
-            @wod.push("1 minute plank")
-            @wod.push("30 seconds rest")
-            @wod.push("30 seconds of pushups")
-            rest
-            @wod.push("1 minute hammer curls")
-            rest
-            @wod.push("1 minute of tricep extension")
-            rest
-            @wod.push("1 minute of lunges")
-            rest
-            @wod.push("30 seconds of pushups")
-        elsif @type == "cardio"
-            @wod.push("1 minute of goblet squats")
-            rest
-            @wod.push("30 seconds of mountain climbers")
-            rest
-            @wod.push("1 minute of squat jumps")
-            rest
-            @wod.push("30 seconds of burpees")
-            rest
-            @wod.push("1 minute wallsit")
-            rest
-            @wod.push("1 minute plank")
-            rest
-            @wod.push("30 seconds of pushups")
-            rest
-            @wod.push("1 minute of jumping jacks")
-        end
-    end
+    # def wodmaker
+    #     if @type == "strength"
+    #         @wod.push("1 minute alternating dumbell snatches")
+    #         rest
+    #         @wod.push("1 minute kettle bell swings")
+    #         rest
+    #         @wod.push("1 minute plank")
+    #         rest
+    #         @wod.push("30 seconds of pushups")
+    #         rest
+    #         @wod.push("1 minute hammer curls")
+    #         rest
+    #         @wod.push("1 minute of tricep extension")
+    #         rest
+    #         @wod.push("1 minute of lunges")
+    #         rest
+    #         @wod.push("30 seconds of pushups")
+    #     elsif @type == "cardio"
+    #         @wod.push("1 minute of goblet squats")
+    #         rest
+    #         @wod.push("30 seconds of mountain climbers")
+    #         rest
+    #         @wod.push("1 minute of squat jumps")
+    #         rest
+    #         @wod.push("30 seconds of burpees")
+    #         rest
+    #         @wod.push("1 minute wallsit")
+    #         rest
+    #         @wod.push("1 minute plank")
+    #         rest
+    #         @wod.push("30 seconds of pushups")
+    #         rest
+    #         @wod.push("1 minute of jumping jacks")
+    #     end
+    # end
     def rounds
         if @length == "thirty"
             return "DO THREE ROUNDS"
@@ -58,13 +62,13 @@ class Workout
     end
     def calcrest
         if @intensity == "hard"
-            @resttime = "30 seconds"
+            @resttime = 30
         elsif @intensity == "medium"
-            @resttime = "45 seconds"
+            @resttime = 45
         elsif @intensity == "easy"
-            @resttime = "1 minute"
+            @resttime = 60
         end
-            
+        @worktime = 120 - @resttime      
     end
     
 end
